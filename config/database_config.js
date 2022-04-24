3431035490319e7ef56595a8fc906f8ae0cad285
sachinbadnikai <38239908+sachinbadnikai@users.noreply.github.com>
2022-04-24 11:45:44 +0530

    Update database_config.js

--- config/database_config.js
+++ config/database_config.js
@@ -1,13 +1,8 @@
 module.exports = {
-    HOST: "database-3.c2sv4xf0su16.ap-south-1.rds.amazonaws.com",
+    HOST: "database-2.c2sv4xf0su16.ap-south-1.rds.amazonaws.com",
+    PORT: '3306',
     USER: "admin",
     PASSWORD: "admin123",
     DB: "awstest",
     dialect: "mysql",
-    pool: {//pool configuration
-      max: 5,//maximum number of connection in pool
-      min: 0,//minimum number of connection in pool
-      acquire: 30000,//maximum time in ms that pool will try to get connection before throwing error
-      idle: 10000//maximum time in ms, that a connection can be idle before being released
-    }
   };
